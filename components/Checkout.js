447b906127abdaa4d754f177b304077ddc809abe
putranugroho <putraa.nugroho@gmail.com>
2025-11-26 09:28:17 +0700

    error UI

--- components/Checkout.js
+++ components/Checkout.js
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 import styles from '../styles/Checkout.module.css'
-import AddPopup from './AddPopUp'
+import AddPopup from './AddPopup'
 import { getCart, updateCart, removeFromCartByIndex, cartPaymentTotal } from '../lib/cart'
 
 function formatRp(n) {

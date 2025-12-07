a9e29816945c1fc576f43477e41640844cedcf57
putranugroho <putraa.nugroho@gmail.com>
2025-12-07 13:49:45 +0700

    test cors

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
   // next.config.js
   const isDev = process.env.NODE_ENV !== 'production';
 
-  const CSP = `
-    default-src 'self';
-    script-src 'self' https://cdn.tailwindcss.com 'unsafe-inline' 'unsafe-eval';
-    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-    img-src 'self' data: blob: https://order.yoshinoya.co.id https://yoshinoya-store-api.akasia.id https://yoshinoya-ho-api.akasia.id http://172.16.100.105:81;
-    font-src 'self' https://fonts.gstatic.com;
-    connect-src 'self' https://yoshinoya-store-api.akasia.id https://yoshinoya-ho-api.akasia.id;
-  `.replace(/\s{2,}/g, " ").trim();
+  const csp = `default-src 'self'; script-src 'self' https://cdn.tailwindcss.com 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.tailwindcss.com; img-src 'self' data: blob: https://yoshinoya-store-api.akasia.id https://yoshinoya-ho-api.akasia.id http://172.16.100.105:81 https://merchants-app.sbx.midtrans.com https://order.yoshinoya.co.id; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://merchants-app.sbx.midtrans.com https://yoshinoya-store-api.akasia.id https://yoshinoya-ho-api.akasia.id https://yoshi-smartqr-api-ergyata5hff3cfhz.southeastasia-01.azurewebsites.net https://cdn.tailwindcss.com https://ab.reasonlabsapi.com https://fonts.gstatic.com; frame-src 'self' https://merchants-app.sbx.midtrans.com;`;
 
   const nextConfig = {
     reactStrictMode: true,
@@ -29,28 +22,10 @@
         {
           source: "/(.*)",
           headers: [
-            {
-              key: "Content-Security-Policy",
-              value: `
-                default-src 'self';
-                script-src 'self' https://cdn.tailwindcss.com 'unsafe-inline' 'unsafe-eval';
-                style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-                img-src 'self' data: blob: https://yoshinoya-store-api.akasia.id https://yoshinoya-ho-api.akasia.id http://172.16.100.105:81 https://merchants-app.sbx.midtrans.com;
-                font-src 'self' https://fonts.gstatic.com;
-
-                connect-src
-                  'self'
-                  https://merchants-app.sbx.midtrans.com
-                  https://yoshinoya-store-api.akasia.id
-                  https://yoshinoya-ho-api.akasia.id
-                  https://yoshi-smartqr-api-ergyata5hff3cfhz.southeastasia-01.azurewebsites.net;
-
-                frame-src 'self' https://merchants-app.sbx.midtrans.com;
-              `.replace(/\s+/g, ' ')
-            }
+            { key: "Content-Security-Policy", value: csp.replace(/\s+/g, ' ') }
           ]
         }
-      ];
+      ]
     }
   };
 

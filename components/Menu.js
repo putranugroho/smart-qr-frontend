a2facc58c91bb9bd7071ba0450651a13ccc5814e
putranugroho <putraa.nugroho@gmail.com>
2025-12-29 09:29:46 +0700

    safeitem

--- components/Menu.js
+++ components/Menu.js
@@ -631,28 +631,22 @@ export default function Menu() {
                   ) : viewMode === "list" ? (
                     <div style={{ display: "flex", flexDirection: "column", gap: 16 }}>
                       {cat.items.map((it) => {
-                        finalItems.forEach(it => {
-  console.log('[FINAL ITEMS]', {
-    id: it.id,
-    name: it.name,
-    outOfStock: it.outOfStock,
-    isCombo: !!it.comboGroups
-  });
-});
+                        const safeItem = {
+                          ...it,
+                          outOfStock: it.outOfStock === true
+                        }
                         return <CardItem key={it.id} item={safeItem} mode={viewMode} />
                       })}
                     </div>
                   ) : (
                     <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gap: 12 }}>
                       {cat.items.map((it) => {
-                        finalItems.forEach(it => {
-  console.log('[FINAL ITEMS]', {
-    id: it.id,
-    name: it.name,
-    outOfStock: it.outOfStock,
-    isCombo: !!it.comboGroups
-  });
-});                     
+                        const safeItem = {
+                          ...it,
+                          outOfStock: it.outOfStock === true
+                        }
+                        console.log("safeItem",safeItem);
+                        
                         return <CardItem key={it.id} item={safeItem} mode={viewMode} />
                       })}
                     </div>
